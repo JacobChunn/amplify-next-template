6170aea0ed8b25bc55f672841ab55a012c4487ff
Jacob Chunn <chunnjake@gmail.com>
2024-12-15 20:03:30 -0500

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
+import "@aws-amplify/ui-react/styles.css"
+import AuthWrapper from "@/components/auth-wrapper";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthWrapper>
+          {children}
+        </AuthWrapper>
+      </body>
     </html>
   );
 }
